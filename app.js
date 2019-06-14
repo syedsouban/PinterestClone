6fe8a8895140ca831bfdb14c79580e782e8ff3ea
Syed Souban <syedsouban890@gmail.com>
2019-06-14 15:14:34 +0530

    signup, login working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 
 
-mongoose.connect(processe.env.MONGO_URI,{useNewUrlParser:true},function (err) {
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true},function (err) {
   
   if (err) throw err
   console.log("Connected to local mongo db database");
